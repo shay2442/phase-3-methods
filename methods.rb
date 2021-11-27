abe912d239370a24472b14a87004835bc55c9d73
shaywacker <shay2442@yahoo.com>
2021-11-27 13:43:53 -0700

    commit tree 12d5feb7691e007b8e4009ae24d3371174eddc5f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name)
+    puts "Hello, #{name}!"
+    if name.class != String
+        return "Hello, programmer!"
+    end
+end 
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+sum1 = add(3,4)
+
+def halve(num1)
+    return num1 /2
+    if name.class != Integer
+        return nil
+    end
+end
